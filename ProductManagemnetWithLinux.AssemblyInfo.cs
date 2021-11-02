000877aa190be53c4a68abe8c689b0e20c06fcd2
NAVYATHA MEKALA <navyatha487@gmail.com>
2021-11-02 14:27:43 +0530

    Product Management Review

---
+++ ProductManagemnetWithLinux.AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("ProductReviewManagementwithLinq")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("ProductReviewManagementwithLinq")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ProductReviewManagementwithLinq")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
