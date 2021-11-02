f898ea7c81017a3454daf03a800351163e37670a
NAVYATHA MEKALA <navyatha487@gmail.com>
2021-11-02 14:46:10 +0530

    To created AddressBook

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AddressBookWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the address book using linq");
+            AddressBookTable addressBook = new AddressBookTable();
+            addressBook.createTable();
+        }
+    }
